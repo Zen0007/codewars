0472ec64246942eda825c565783c5cfc44b7e83a
Zen <zmr8388@gmail.com>
2024-03-31 17:11:18 +0700

    chelleng 28

---
+++ codewars-34.dart
@@ -0,0 +1,25 @@
+//https://www.codewars.com/kata/59b844528bcb7735560000a0/train/dart
+String oddOrEven1(List<int> array) {
+  int sum = array.reduce((a, b) {
+    return b >= 0 ? a + b : a;
+  });
+  return sum.isEven ? "even" : "odd";
+}
+
+String oddOrEven(List<int> array) => array.fold(0, (a, b) {
+      return b >= 0 ? a + b : a;
+    }).isEven
+        ? "even"
+        : "odd";
+
+String oddOrEven2(List<int> array) {
+  int sum = 0;
+  for (var i in array) {
+    sum += i;
+  }
+  return sum.isEven ? "even" : "odd";
+}
+
+void main() {
+  print(oddOrEven([-100, 99]));
+}
